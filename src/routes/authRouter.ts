45370989f7c241bd01010a833d1f75ffedb6e3c3
c.giacometti <camilla.giacometti@gmail.com>
2022-09-11 20:45:53 -0300

    chore: adjust to deploy

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -3,6 +3,9 @@ import { createUser, loginUser } from "../controllers/authController.js";
 
 const router = Router();
 
+router.get("/", (req, res) => {
+    res.send("aqui")
+})
 router.post("/signup", createUser);
 router.post("/signin", loginUser);
 
